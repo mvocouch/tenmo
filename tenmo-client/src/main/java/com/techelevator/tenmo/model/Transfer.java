d4dada712de072ba59f26f4b883651cf658f48a4
Ryan Rosa <ryan.rosa.829@gmail.com>
2023-12-08 23:25:45 -0500

    Created Client-side Account & Transfer Model classes, Created UserService class, Added @DecimalMax to amount in Server-side Transfer

--- tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -1,39 +1,17 @@
 package com.techelevator.tenmo.model;
 
-
-import org.springframework.data.annotation.Id;
-
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class Transfer {
-    @NotNull
-    @Id
     private long id;
-    @NotNull
     private int transferStatus;
-    @NotNull
     private int accountFrom;
-    @NotNull
     private int accountTo;
-    @NotNull
-    @DecimalMin(value = ".01")
-    @DecimalMax(value = "9999999999999.99")
     private BigDecimal amount;
 
-    public Transfer(long id, int transfer_status, int accountFrom, int accountTo, BigDecimal amount) {
+    public Transfer(long id, int transferStatus, int accountFrom, int accountTo, BigDecimal amount) {
         this.id = id;
-        this.transferStatus = transfer_status;
-        this.accountFrom = accountFrom;
-        this.accountTo = accountTo;
-        this.amount = amount;
-    }
-
-    public Transfer(int transfer_status, int accountFrom, int accountTo, BigDecimal amount) {
-        this.transferStatus = transfer_status;
+        this.transferStatus = transferStatus;
         this.accountFrom = accountFrom;
         this.accountTo = accountTo;
         this.amount = amount;
@@ -79,8 +57,17 @@ public class Transfer {
     }
 
     public void setAmount(BigDecimal amount) {
-        this.amount = amount.setScale(2, RoundingMode.HALF_DOWN);
+        this.amount = amount;
     }
 
-
+    @Override
+    public String toString() {
+        return "Transfer{" +
+                "id = " + id +
+                ", transferStatus = " + transferStatus +
+                ", accountFrom = " + accountFrom +
+                ", accountTo = " + accountTo +
+                ", amount = " + amount +
+                '}';
+    }
 }
---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/UserService.java
@@ -0,0 +1,85 @@
+package com.techelevator.tenmo.services;
+
+import com.techelevator.tenmo.model.User;
+import com.techelevator.util.BasicLogger;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+public class UserService {
+
+    private final String baseUrl;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public UserService(String url){
+        this.baseUrl = url;
+    }
+
+    private  String authToken = null;
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public User getUser(int id){
+        User user = null;
+        try{
+            ResponseEntity<User> response =
+                    restTemplate.exchange(baseUrl + id, HttpMethod.GET, makeAuthEntity(), User.class);
+            user = response.getBody();
+        } catch (RestClientResponseException | ResourceAccessException e){
+            BasicLogger.log(e.getMessage());
+        }
+        return  user;
+    }
+
+    public User addUser(User newUser){
+        HttpEntity<User> entity = makeUserEntity(newUser);
+        User returnedUser = null;
+        try {
+            returnedUser = restTemplate.postForObject(baseUrl, entity, User.class);
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return returnedUser;
+    }
+
+    public boolean updateUser(User updateedUser){
+        HttpEntity<User> entity = makeUserEntity(updateedUser);
+        boolean success = false;
+        try {
+            restTemplate.put(baseUrl + updateedUser.getId(), entity);
+            success =true;
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return success;
+    }
+
+    public boolean deleteUser(int userId){
+        boolean success = false;
+        try {
+            restTemplate.exchange(baseUrl + userId, HttpMethod.DELETE, makeAuthEntity(), Void.class);
+            success = true;
+        } catch (RestClientResponseException | ResourceAccessException e) {
+            BasicLogger.log(e.getMessage());
+        }
+        return success;
+    }
+
+    private HttpEntity<User> makeUserEntity(User user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(user, headers);
+    }
+
+    private HttpEntity<Void> makeAuthEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(authToken);
+        return new HttpEntity<>(headers);
+    }
+
+
+}
